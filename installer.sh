e751d2ae4f75927d1b3fa2e935c80fdd96a538b9
Gagan Malvi <malvigagan@gmail.com>
2023-06-18 22:23:48 +0530

    Initial commit
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

---
+++ installer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Installing the following System76 packages:
+# - system76-power
+# - system76-firmware
+# - system76-driver
+# - firmware-manager
+# THe script is optimized for the Lemur Pro with the Alder Lake CPU (lemp11)
